3e25b6f2a25df1b6bb04b1e25c292fb584e542e1
Nemocdz <nemocdz@gmail.com>
2018-11-09 21:57:06 +0800

    add problems

---
+++ 921. Minimum Add to Make Parentheses Valid.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+let s = "())"
+
+class Solution {
+    func minAddToMakeValid(_ S: String) -> Int {
+        var i = 0
+        var j = S.count - 1
+        var result = 0
+        while i != j {
+            let a = String.Index.init(encodedOffset: i)
+            let b = String.Index.init(encodedOffset: j)
+  
+            
+            if S[a] == "(" && S[b] == ")" {
+                i += 1
+                j -= 1
+            } else if S[a] == "(" {
+                i += 1
+                result += 1
+            } else if S[b] == ")" {
+                j -= 1
+                result += 1
+            } else {
+                i += 1
+                j -= 1
+                result += 2
+            }
+            
+        }
+        return result
+    }
+}
+
+Solution().minAddToMakeValid(s)
