fa40a37bf68d0fe3f44e0274ed063c0b3949631d
Nemocdz <nemocdz@gmail.com>
2019-02-25 11:05:14 +0800

    add problem

--- 95. Unique Binary Search Trees II.playground/Contents.swift
+++ 95. Unique Binary Search Trees II.playground/Contents.swift
@@ -16,6 +16,46 @@ public class TreeNode {
  
 class Solution {
     func generateTrees(_ n: Int) -> [TreeNode?] {
-        
+        if n == 0 {
+            return []
+        }
+        let nums = (1..<n + 1).map{$0}
+        return generateTrees(nums)
+    }
+    
+    func generateTrees(_ nums: [Int]) -> [TreeNode?] {
+        var result = [TreeNode]()
+        for i in nums {
+            let (small, big) = nums.split(i)
+            let smalls = generateTrees(small)
+            let bigs = generateTrees(big)
+            for s in smalls {
+                for b in bigs {
+                    let root = TreeNode(i)
+                    root.left = s
+                    root.right = b
+                    result.append(root)
+                }
+            }
+        }
+        return result.isEmpty ? [nil] : result
     }
 }
+
+extension Array where Element == Int{
+    func split(_ x:Int) -> ([Int], [Int]) {
+        var small = [Int]()
+        var big = [Int]()
+        for num in self {
+            if num > x {
+                big.append(num)
+            } else if num < x {
+                small.append(num)
+            }
+        }
+        return (small, big)
+    }
+}
+
+Solution().generateTrees(3)
+
