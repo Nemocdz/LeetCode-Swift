4bbc776af388a9e8082ee9a307c9f11101e3fb3b
Nemocdz <nemocdz@gmail.com>
2019-05-19 14:21:07 +0800

    fix problem

--- 209. Minimum Size Subarray Sum.playground/Contents.swift
+++ 209. Minimum Size Subarray Sum.playground/Contents.swift
@@ -15,16 +15,34 @@ import Cocoa
 class Solution {
     func minSubArrayLen(_ s: Int, _ nums: [Int]) -> Int {
         var sums = [Int](repeating: 0, count: nums.count + 1)
-        for i in 1..<nums.count + 1 {
+        for i in 1..<sums.count {
             sums[i] = sums[i - 1] + nums[i - 1]
         }
         var i = nums.count
         var length = Int.max
-        while i >= 0 && sums[i] >= s {
-            if let j = sums.firstIndex(where: { $0 > sums[i] - s}) {
-                length = min(length, i - j + 1)
+        
+        func binarySearch(_ left:Int, _ right:Int, _ nums:[Int], _ target:Int) -> Int? {
+            var aLeft = left
+            var aRight = right
+            var middle = 0
+            while aLeft <= aRight {
+                middle = (aLeft + aRight) / 2
+                if nums[middle] < target {
+                    aLeft = middle + 1
+                } else {
+                    aRight = middle - 1
+                }
             }
-            i -= 1
+            return aLeft > right ? nil : aLeft
+        }
+        
+        
+        for i in 0..<sums.count {
+            if let end = binarySearch(i + 1, sums.count - 1, sums, sums[i] + s) {
+                length = min(length, end - i)
+            } else {
+                break
+            }
         }
         return length == Int.max ? 0 : length
     }
