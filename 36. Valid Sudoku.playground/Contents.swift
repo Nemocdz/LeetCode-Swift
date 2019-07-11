8b759abcfe2879f6516a602b13bba32a01c0ad24
Nemocdz <nemocdz@gmail.com>
2019-07-11 20:24:35 +0800

    update

--- 36. Valid Sudoku.playground/Contents.swift
+++ 36. Valid Sudoku.playground/Contents.swift
@@ -1,5 +1,18 @@
 import Cocoa
 
+/*
+ 判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
+ 
+ 数字 1-9 在每一行只能出现一次。
+ 数字 1-9 在每一列只能出现一次。
+ 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+ 
+ 
+ 上图是一个部分填充的有效的数独。
+ 
+ 数独部分空格内已填入了数字，空白格用 '.' 表示。
+*/
+
 class Solution {
     func isValidSudoku(_ board: [[Character]]) -> Bool {
         var rows = [[Bool]](repeating: [Bool](repeating: false, count: 9), count: 9)
--- 49. Group Anagrams.playground/Contents.swift
+++ 49. Group Anagrams.playground/Contents.swift
@@ -1,5 +1,19 @@
 import Cocoa
 
+/*
+ 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ 
+ 示例:
+ 
+ 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ 输出:
+ [
+ ["ate","eat","tea"],
+ ["nat","tan"],
+ ["bat"]
+ ]
+*/
+
 class Solution {
     func groupAnagrams(_ strs: [String]) -> [[String]] {
         var dic = [[Character]:[String]]()
