f385d4c263a3a20b588112e022d589f885233325
Nemocdz <nemocdz@gmail.com>
2020-01-14 20:08:08 +0800

    update solutions

--- 33. Search in Rotated Sorted Array.playground/Contents.swift
+++ 33. Search in Rotated Sorted Array.playground/Contents.swift
@@ -25,14 +25,12 @@ let a = [4, 5, 6, 7, 0, 1, 2]
 
 class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
-        var result = -1
         var start = 0
-        var end  = nums.count - 1
+        var end = nums.count - 1
         while start <= end {
-            let mid = (start + end) / 2
+            let mid = start + (end - start) / 2
             if nums[mid] == target {
-                result = mid
-                break
+                return mid
             }
             
             if nums[start] <= nums[mid] {
@@ -43,14 +41,13 @@ class Solution {
                 }
             } else {
                 if target > nums[mid] && target <= nums[end] {
-                    start = mid + 1
+                    start += 1
                 } else {
                     end = mid - 1
                 }
             }
-            
         }
-        return result
+        return -1
     }
 }
 
