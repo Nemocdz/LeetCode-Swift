1898ee009eb74866e490a3b56e59b2a60cb166ec
Nemocdz <nemocdz@gmail.com>
2020-01-15 18:21:09 +0800

    update solutions

--- 34. Find First and Last Position of Element in Sorted Array.playground/Contents.swift
+++ 34. Find First and Last Position of Element in Sorted Array.playground/Contents.swift
@@ -20,29 +20,42 @@ import Cocoa
 let a = [5, 7, 7, 8, 8, 10]
 class Solution {
     func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
-        guard !nums.isEmpty else {
+        if nums.isEmpty {
             return [-1, -1]
         }
-        let left = findIndex(nums, target, false)
-        if left == nums.count || nums[left] != target  {
-            return [-1, -1]
-        }
-        let right = findIndex(nums, target, true) - 1
-        return [left, right]
-    }
-    
-    func findIndex(_ nums:[Int], _ target:Int, _ isBigger:Bool) -> Int{
-        var start = 0
-        var end = nums.count
-        while start < end {
-            let mid = (start + end) / 2
-            if nums[mid] > target || (!isBigger && nums[mid] == target) {
-                end = mid
+        
+        func binarySearch(_ nums:[Int], target:Int, left:Bool) -> Int? {
+            var start = 0
+            var end = nums.count - 1
+            while start <= end {
+                let mid = (start + end) / 2
+                if nums[mid] > target {
+                    end = mid - 1
+                } else if nums[mid] < target {
+                    start = mid + 1
+                } else {
+                    if left {
+                        end = mid - 1
+                    } else {
+                        start = mid + 1
+                    }
+                }
+            }
+            
+            // 不越界去比较
+            if left {
+                return start <= nums.count - 1 && nums[start] == target ? start : nil
             } else {
-                start = mid + 1
+                return end >= 0 && nums[end] == target ? end : nil
             }
         }
-        return start
+        
+        if let left = binarySearch(nums, target: target, left: true),
+            let right = binarySearch(nums, target: target, left: false) {
+            return [left, right]
+        }
+        
+        return [-1, -1]
     }
 }
 
